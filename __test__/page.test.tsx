4ee6ca91b951223721a87b0272595ee6bbb76c11
Naufal F <naufalfirman098@gmail.com>
2025-03-14 20:05:48 +0700

    feat: add about page

--- __test__/page.test.tsx
+++ __test__/page.test.tsx
@@ -2,6 +2,7 @@
  * @jest-environment jsdom
  */
 import Home from "@/app/page";
+import About from "@/app/about/page";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 
@@ -12,5 +13,13 @@ describe("Home", () => {
 		const heading = screen.getByRole("heading", { level: 1 });
 
 		expect(heading).toBeInTheDocument();
+	});
+});
+
+describe("About", () => {
+	it("renders about text", () => {
+		render(<About />);
+		const aboutElement = screen.getByText("About");
+		expect(aboutElement).toBeInTheDocument();
 	});
 });
--- __test__/snapshot.tsx
+++ __test__/snapshot.tsx
@@ -2,9 +2,15 @@
  * @jest-environment jsdom
  */
 import Home from "@/app/page";
+import About from "@/app/about/page";
 import { render } from "@testing-library/react";
 
 it("renders homepage unchanged", () => {
 	const { container } = render(<Home />);
+	expect(container).toMatchSnapshot();
+});
+
+it("renders about page unchanged", () => {
+	const { container } = render(<About />);
 	expect(container).toMatchSnapshot();
 });
