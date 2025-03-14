b3d961a9ae2287a47893e722dff089d5129e42b7
Naufal F <naufalfirman098@gmail.com>
2025-03-14 22:21:21 +0700

    feat(shop): add shop page

---
+++ app/shop/page.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+const Shop = () => {
+	return (
+		<div>
+			<h1>Shop</h1>
+			<div>
+				<Link href="/">Back to Home</Link>
+			</div>
+		</div>
+	);
+};
+
+export default Shop;
