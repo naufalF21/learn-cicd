a42024624cb06ee69fdf63c70c7d1fcc316be8ff
Naufal F <naufalfirman098@gmail.com>
2025-03-14 19:54:45 +0700

    fix: no error again please

--- __test__/page.test.tsx
+++ __test__/page.test.tsx
@@ -9,9 +9,7 @@ describe("Home", () => {
 	it("renders a heading", () => {
 		render(<Home />);
 
-		const heading = screen.getByRole("heading", {
-			name: /welcome to next\.js!/i,
-		});
+		const heading = screen.getByRole("heading", { level: 1 });
 
 		expect(heading).toBeInTheDocument();
 	});
